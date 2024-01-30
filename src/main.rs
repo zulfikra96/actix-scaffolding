ef52bcda96027b00095725104eaeadc7ec0350a8
zulfikra96 <zulfikralahmudin@gmail.com>
2024-01-30 21:52:00 +0800

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
+use dotenv::dotenv;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "DELETE"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+        App::new()
+            .wrap(cors) 
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
